eb37056b57470b987e9e1bf2959b0b31e0cfaa24
unknown <visquilliam@gmail.com>
2021-06-12 18:53:29 -0300

    William: Classe Manutenção

--- src/mecanica/model/domain/Veiculo.java
+++ src/mecanica/model/domain/Veiculo.java
@@ -1,62 +1,58 @@
 package mecanica.model.domain;
 
-
 import java.io.Serializable;
 
-/**
- *
- * @author Jones
- */
-public class Veiculo implements Serializable{
+public class Veiculo implements Serializable {
+
     private String placa;
     private String nome;
     private String marca;
     private int cdModelo;
     private String cdCliente;
 
-    public String getPlaca(){
+    public String getPlaca() {
         return placa;
     }
 
-    public void setPlaca(String placa){
+    public void setPlaca(String placa) {
         this.placa = placa;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getMarca(){
+    public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca){
+    public void setMarca(String marca) {
         this.marca = marca;
     }
 
-    public int getcdModelo(){
+    public int getcdModelo() {
         return cdModelo;
     }
 
-    public void setcdModelo(int cdModelo){
+    public void setcdModelo(int cdModelo) {
         this.cdModelo = cdModelo;
     }
 
-    public String getcdCliente(){
+    public String getcdCliente() {
         return cdCliente;
     }
 
-    public void setcdCliente(String cdCliente){
+    public void setcdCliente(String cdCliente) {
         this.cdCliente = cdCliente;
     }
 
     @Override
-    public String toString(){
-        return String.format("(Placa: %s, Nome: %s, Marca: %s, Cod_Modelo: %d, Cod_Cliente: %s)", 
+    public String toString() {
+        return String.format("(Placa: %s, Nome: %s, Marca: %s, Cod_Modelo: %d, Cod_Cliente: %s)",
                 placa, nome, marca, cdModelo, cdCliente);
     }
 }
