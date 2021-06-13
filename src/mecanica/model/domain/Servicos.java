36af92196542cd0354855442b48ec9de1e9a9e9c
whoisn0y <d3pths@protonmail.ch>
2021-06-12 22:26:31 -0300

    Andrey: Classe Serviço

---
+++ src/mecanica/model/domain/Servicos.java
@@ -0,0 +1,50 @@
+package mecanica.model.domain;
+
+import java.io.Serializable;
+
+public class Servicos implements Serializable {
+
+    private Integer cdServico;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public int cdServico() {
+        return cdServico;
+    }
+
+    public void setcdServico(int cdServico) {
+        this.cdServico = cdServico;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public Double Preco(){
+        return this.preco;
+    }
+    
+    public void setPreco(Double preco){
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("(Codigo: %d, Nome: %s, Descrição: %s, Preço: %.2f)",
+                cdServico, nome, descricao, preco);
+    }
+}
